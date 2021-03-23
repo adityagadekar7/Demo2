cc3ffb9fb0bdf66f8301c143f61006c8ef2ef409
adityagadekar7 <adityagadekar07@gmail.com>
2021-03-23 11:00:04 +0530

    Add project files.

---
+++ Demo1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        public void function1()
+        {
+            Console.WriteLine("Function 1");
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
